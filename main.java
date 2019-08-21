6f24c5396e6d10963fb705d07bbaba0ca66bc284
Satato <amber.melton.2020@gmail.com>
2019-08-21 09:43:58 -0400

    import java.math.*;
    see file;

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
+//imports math for use with math functions and Math.random() throughout the program
+import java.math.*;
 //sets up the main class
 public class main {
 //sets up the 'main'; aka what first runs when the program is run

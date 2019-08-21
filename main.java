958f77d21a7011eb771fa542c1220334921127af
Satato <amber.melton.2020@gmail.com>
2019-08-21 10:01:59 -0400

    added cards, imported arraylists, etc.
    see main.java;

--- main.java
+++ main.java
@@ -1,8 +1,16 @@
 //imports math for use with math functions and Math.random() throughout the program
 import java.math.*;
+import java.util.ArrayList;
+
 //sets up the main class
 public class main {
-//sets up the 'main'; aka what first runs when the program is run
+	//defines global variables for use throughout the class
+	//defines the 'cards' array containing all possible card identities in a deck of cards
+	String[] cards = {"Ace-H","2-H","3-H","4-H","5-H","6-H","7-H","8-H","9-H","10-H","Jack-H","Queen-H","King-H","Ace-S","2-S","3-S","4-S","5-S","6-S","7-S","8-S","9-S","10-S","Jack-S","Queen-S","King-S","Ace-D","2-D","3-D","4-D","5-D","6-D","7-D","8-D","9-D","10-D","Jack-D","Queen-D","King-D","Ace-C","2-C","3-C","4-C","5-C","6-C","7-C","8-C","9-C","10-C","Jack-C","Queen-C","King-C"};
+	//prepares the user's hand and the computer's hand to be given cards
+	ArrayList[] hpHand = new ArrayList[7];
+	ArrayList[] cpHand = new ArrayList[7];
+	//sets up the 'main'; aka what first runs when the program is run
 	public static void main(String[] args) {
 
 	}
